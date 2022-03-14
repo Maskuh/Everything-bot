4c7ee110479aaed606869e6b8ce1e2e65b8315b6
Maskuh <85193434+Maskuh@users.noreply.github.com>
2022-03-14 13:22:18 -0700

    Updates to be able to be added to certain servers

--- cogs/commands.py
+++ cogs/commands.py
@@ -10,7 +10,6 @@ class CMS2(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-
 # Ping command
     @commands.command(name='Ping')
     async def Ping(self, ctx):
@@ -42,13 +41,24 @@ class CMS2(commands.Cog):
 # Help Command
     @commands.command(name='Help')
     async def Help(self, ctx):
-        embed=discord.Embed(title='Help Page!', description=f"%Ping (To tell you if I am online or not) \n %Purge (To purge mass messages) \n %Invite (Opens the invite page to invite me!) \n %Help (To open this page to tell you what commands I have)", color=0x206694)
+        embed=discord.Embed(title='Help Page!', description=f"%Ping (To tell you if I am online or not) \n %Purge (To purge mass messages (Must have the manage message permission!!)) \n %Invite (Opens the invite page to invite me!) \n %Help (To open this page to tell you what commands I have) \n %Bal (Check you balence in the economy) \n %bag (Check your inventory to see what you have) \n %beg (Beg for money in the economy) \n %number(generates a random number for any of your needs) \n %Leave (Makes me leave the server(Note Only user <@786788350160797706> can as they are the Developer of the bot)) \n %shop (opens the shop in the economy) \n %buy (buys something from the shop) \n %Sell (sells an item from your bag) \n %use (command is not implemented yet and is in production sorry for the inconvience) \n %Slots (gamble your money)", color=0x206694)
         await ctx.channel.send(embed=embed)
 #Invite command
     @commands.command(name='Invite')
     async def Invite(self, ctx):
         embed=discord.Embed(title='Invite Links!', description=f"[Invite(recommended)](https://discord.com/api/oauth2/authorize?client_id=902249858281394237&permissions=274877999168&scope=bot) \n [Invite(admin)](https://discord.com/api/oauth2/authorize?client_id=902249858281394237&permissions=8&scope=bot)", color=0x2ecc71)
         await ctx.channel.send(embed=embed)
+
+#Leave command
+    @commands.command(name='Leave')
+    async def Leave(self,ctx):
+        if ctx.author.id == 786788350160797706:
+            await ctx.channel.send("Very well, idk why you would want me to leave but ok bye")
+            await asyncio.sleep(1)
+            await ctx.guild.leave()
+        else:
+            await ctx.channel.send("You can't use this command only the owner of the bot can!")
+            
 
 
 def setup(client):
